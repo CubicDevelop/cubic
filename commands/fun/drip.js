674328da147aa32692e78d1d4f9b180eaa3c94e8
svcodes <suhasvoolla@gmail.com>
2021-01-11 15:44:30 -0600

    add drip

---
+++ commands/fun/drip.js
@@ -0,0 +1,18 @@
+module.exports = {
+    name: "drip",
+    aliases: ['gokudrip'],
+    catergory: "fun",
+    description: "gives you yourself but with ***drip***",
+    usage: "drip [user]",
+    run: async (Discord, bot, message, args) => {
+        const user = message.mentions.users.first();
+        const embed = new Discord.MessageEmbed()
+                    .setTitle("D R I P")
+                    .setImage(`https://vacefron.nl/api/user?avatar=${user.displayAvatarUrl() ?? message.author.displayAvatarUrl()}`)
+                    .setColor('RANDOM')
+                    .setTimestamp();
+                    message.channel.send(embed);
+  }
+  }
+  
+  
